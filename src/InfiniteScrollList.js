266f9c10799fea7335dccf2cad603b6b1a67e6fe
Damon Shen <shi-min.shen@oath.com>
2019-10-02 19:05:49 +0800

    refactor

--- src/InfiniteScrollList.js
+++ src/InfiniteScrollList.js
@@ -1,16 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react'
 
-const useOnScreen = (callback, rootMargin = '50px') => {
+const useSentinel = (callback, rootMargin = '0px') => {
+  const [isFetching, setIsFetching] = useState(false)
   const ref = useRef()
-  // State and setter for storing whether element is visible
   useEffect(
     () => {
       const { current } = ref
       const observer = new IntersectionObserver(
         ([entry]) => {
-          // Update our state when observer callback fires
-          console.log(entry);
-          callback && callback(entry.isIntersecting)
+          callback && callback(entry.isIntersecting, isFetching, setIsFetching)
         },
         {
           rootMargin
@@ -24,7 +22,7 @@ const useOnScreen = (callback, rootMargin = '50px') => {
         observer.unobserve(current)
       }
     },
-    [callback, ref, rootMargin]
+    [callback, isFetching, ref, rootMargin]
   )
 
   return ref
@@ -36,55 +34,62 @@ const fetchMockData = (mockData = 'bottomAddItem') => {
       resolve(mockData)
     }, 1000)
   })
-};
+}
 
 const useInfiniteScroll = ({ topCallback, bottomCallback }) => {
-
-  const [isFetching, setIsFetching] = useState(false)
   const listRef = useRef()
 
-  const topSentinelRef = useOnScreen((onScreen) => {
+  const topSentinelRef = useSentinel((onScreen, isFetching, setIsFetching) => {
     if (!isFetching && onScreen) {
       setIsFetching(true)
       topCallback && topCallback(listRef, topSentinelRef, setIsFetching)
     }
   })
-  const bottomSentinelRef = useOnScreen((onScreen) => {
-    if (!isFetching && onScreen) {
-      setIsFetching(true)
-      bottomCallback && bottomCallback(listRef, bottomSentinelRef, setIsFetching)
+  const bottomSentinelRef = useSentinel(
+    (onScreen, isFetching, setIsFetching) => {
+      if (!isFetching && onScreen) {
+        setIsFetching(true)
+        bottomCallback &&
+          bottomCallback(listRef, bottomSentinelRef, setIsFetching)
+      }
     }
-  })
+  )
 
   return { listRef, topSentinelRef, bottomSentinelRef }
 }
 
 export default () => {
-  const [list, setList] = useState(['test']);
+  const [list, setList] = useState(['test'])
   const topCallback = (listRef, sentinelRef, setIsFetching) => {
-    fetchMockData('topAddItem').then((data) => {
+    fetchMockData('topAddItem').then(data => {
       const originScrollHeight = listRef.current.scrollHeight
-      setList(list => ([data, ...list]))
-      listRef.current.scrollTop = sentinelRef.current.clientHeight + listRef.current.scrollHeight - originScrollHeight
+      setList(list => [data, ...list])
+      // add 1px to prevent IntersectionObserver trigged again
+      listRef.current.scrollTop =
+        listRef.current.scrollHeight - originScrollHeight + 1
+      setIsFetching(false)
+    })
+  }
+
+  const bottomCallback = (listRef, sentinelRef, setIsFetching) => {
+    fetchMockData().then(data => {
+      setList(list => [...list, data])
       setIsFetching(false)
     })
   }
 
   const { listRef, topSentinelRef, bottomSentinelRef } = useInfiniteScroll({
     topCallback,
-    bottomCallback: (listRef, sentinelRef, setIsFetching) => {
-      fetchMockData().then((data) => {
-        setList(list => ([...list, data]))
-        setIsFetching(false)
-      })
-    }
+    bottomCallback
   })
 
   return (
     <div ref={listRef} style={{ height: '200px', overflow: 'auto' }}>
-      <div ref={topSentinelRef}>top</div>
-      {list.map((item, idx) => (<div key={idx}>{item}</div>))}
-      <div ref={bottomSentinelRef}>bottom</div>
+      <div ref={topSentinelRef}>Loading</div>
+      {list.map((item, idx) => (
+        <div key={idx}>{item}</div>
+      ))}
+      <div ref={bottomSentinelRef}>Loading</div>
     </div>
   )
 }
