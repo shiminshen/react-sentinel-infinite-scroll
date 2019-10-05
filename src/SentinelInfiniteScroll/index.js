89be542bb27d48818c3060c43a1f90c8aafbb3c9
Damon Shen <shi-min.shen@oath.com>
2019-10-05 14:37:11 +0800

    add observerOptions

--- src/SentinelInfiniteScroll/index.js
+++ src/SentinelInfiniteScroll/index.js
@@ -1,16 +1,14 @@
 import { useEffect, useRef, useState } from 'react'
 
-export const useSentinel = (callback) => {
+export const useSentinel = (callback, observerOptions) => {
   const [isFetching, setIsFetching] = useState(false)
   const ref = useRef()
   useEffect(
     () => {
       const { current } = ref
-      const observer = new IntersectionObserver(
-        ([entry]) => {
-          callback && callback(entry.isIntersecting, isFetching, setIsFetching)
-        }
-      )
+      const observer = new IntersectionObserver(([entry]) => {
+        callback && callback(entry.isIntersecting, isFetching, setIsFetching)
+      }, observerOptions)
 
       if (current) {
         observer.observe(current)
@@ -19,13 +17,18 @@ export const useSentinel = (callback) => {
         observer.disconnect()
       }
     },
-    [callback, isFetching, ref]
+    [callback, isFetching, observerOptions, ref]
   )
 
   return ref
 }
 
-export const useInfiniteScroll = ({ topCallback, bottomCallback }) => {
+export const useInfiniteScroll = ({
+  topCallback,
+  topObserverOptions,
+  bottomCallback,
+  bottomObserverOptions
+}) => {
   const listRef = useRef()
 
   const topSentinelRef = useSentinel((onScreen, isFetching, setIsFetching) => {
@@ -33,7 +36,7 @@ export const useInfiniteScroll = ({ topCallback, bottomCallback }) => {
       setIsFetching(true)
       topCallback && topCallback(listRef, topSentinelRef, setIsFetching)
     }
-  })
+  }, topObserverOptions)
 
   const bottomSentinelRef = useSentinel(
     (onScreen, isFetching, setIsFetching) => {
@@ -42,18 +45,14 @@ export const useInfiniteScroll = ({ topCallback, bottomCallback }) => {
         bottomCallback &&
           bottomCallback(listRef, bottomSentinelRef, setIsFetching)
       }
-    }
+    },
+    bottomObserverOptions
   )
 
   return { listRef, topSentinelRef, bottomSentinelRef }
 }
 
-export default ({
-  className,
-  children,
-  topCallback,
-  bottomCallback
-}) => {
+export default ({ className, children, topCallback, bottomCallback }) => {
   const { listRef, topSentinelRef, bottomSentinelRef } = useInfiniteScroll({
     topCallback,
     bottomCallback
